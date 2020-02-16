1e831052e397a07b9dece6326eb0cfb6a7463937
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-02-16 17:17:55 -0500

    Binarize dataset

---
+++ binarize-dataset.sh
@@ -0,0 +1,11 @@
+export DATA_PATH=/mnt/nfs/work1/mccallum/abajaj/gs-summ/data/cnn-for-bart
+
+fairseq-preprocess \
+  --source-lang "source" \
+  --target-lang "target" \
+  --trainpref "$DATA_PATH/train.bpe" \
+  --validpref "$DATA_PATH/val.bpe" \
+  --destdir "$DATA_PATH/cnn_dm-bin/" \
+  --workers 60 \
+  --srcdict dict.txt \
+  --tgtdict dict.txt;
