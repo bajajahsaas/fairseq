a9d3e6ca181e5c6a310d9ef2062d1e254aec76ff
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-02-17 16:14:44 -0500

    Change checkpoint writing path

--- finetune.sh
+++ finetune.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=finetune-bart
 #SBATCH --output=logsfinetunebart/bart_%j.txt  # output file
 #SBATCH -e logsfinetunebart/bart_%j.err        # File to which STDERR will be written
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:3
 #SBATCH --partition=2080ti-long # Partition to submit to
 #SBATCH --mem=40G
 #
@@ -16,8 +16,10 @@ MAX_TOKENS=2048
 UPDATE_FREQ=4
 BART_PATH=/mnt/nfs/work1/mccallum/abajaj/gs-summ/models/bart.large/model.pt
 PROCESSED_DATA_PATH=/mnt/nfs/work1/mccallum/abajaj/gs-summ/data/cnn-for-bart/cnn_dm-bin
+SAVE_DIR=/mnt/nfs/work1/696ds-s20/abajaj/cnn-finetune
 
 python train.py $PROCESSED_DATA_PATH \
+    --save_dir $SAVE_DIR \
     --restore-file $BART_PATH \
     --max-tokens $MAX_TOKENS \
     --task translation \
